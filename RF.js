ba80406d7e177be021724d9378fb437bc5d4e853
Prathyusha2212 <120353823+Prathyusha2212@users.noreply.github.com>
2022-12-14 12:57:08 +0530

    Add files via upload

---
+++ RF.js
@@ -0,0 +1,103 @@
+function check(){
+    let p1;
+    let p2;
+    let p3;
+    let p4;
+    let p5;
+    let p6;
+    let p7;
+    let p8;
+    let p9;
+    let p10;
+    let p11;
+    let p12;
+    let p13;
+    let p14;
+    let p15;
+    let p16;
+    let p17;
+    let p18;
+    let p19;
+    let p20;
+    let p21;
+    let p22;
+    let p23;
+
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+    console.log(p4);
+    console.log(p5);
+    console.log(p6);
+    console.log(p7);
+    console.log(p8);
+    console.log(p9);
+    console.log(p10);
+    console.log(p11);
+    console.log(p12);
+    console.log(p13);
+    console.log(p14);
+    console.log(p15);
+    console.log(p16);
+    console.log(p17);
+    console.log(p18);
+    console.log(p19);
+    console.log(p20);
+    console.log(p21);
+    console.log(p22);
+    console.log(p23);
+
+
+    //storing values
+
+    p1=document.getElementById("f1").Value;
+    p2=document.getElementById("f2").Value;
+    p3=document.getElementById("f3").Value;
+    p4=document.getElementById("f4").Value;
+    p5=document.getElementById("f5").Value;
+    p6=document.getElementById("f6").Value;
+    p7=document.getElementById("f7").Value;
+    p8=document.getElementById("f8").Value;
+    p9=document.getElementById("f9").Value;
+    p10=document.getElementById("f10").Value;
+    p11=document.getElementById("f11").Value;
+    p12=document.getElementById("f12").Value;
+    p13=document.getElementById("f13").Value;
+    p14=document.getElementById("f14").Value;
+    p15=document.getElementById("f15").Value;
+    p16=document.getElementById("f16").Value;
+    p17=document.getElementById("f17").Value;
+    p18=document.getElementById("f18").Value;
+    p19=document.getElementById("f19").Value;
+    p20=document.getElementById("f20").Value;
+    p21=document.getElementById("f21").Value;
+    p22=document.getElementById("f22").Value;
+    p23=document.getElementById("f23").Value;
+
+    //showing values
+    document.getElementById("fname").innerHTML=p1;
+    document.getElementById("fmname").innerHTML=P2;
+    document.getElementById("flname").innerHTML=P3;
+    document.getElementById("fmail").innerHTML=P4;
+    document.getElementById("fpd").innerHTML=P5;
+    document.getElementById("fdob").innerHTML=P6;
+    document.getElementById("fgen").innerHTML=P7;
+    document.getElementById("fadd").innerHTML=P8;
+    document.getElementById("fpc").innerHTML=P9;
+    document.getElementById("fmbno").innerHTML=P10;
+    document.getElementById("fadno").innerHTML=P11;
+    document.getElementById("fpcno").innerHTML=P12;
+    document.getElementById("fcy").innerHTML=P13;
+    document.getElementById("fsts").innerHTML=P14;
+    document.getElementById("fssc mr").innerHTML=P15;
+    document.getElementById("fsc name").innerHTML=P16;
+    document.getElementById("fgd").innerHTML=P17;
+    document.getElementById("finter mr").innerHTML=P18;
+    document.getElementById("fclgname").innerHTML=P19;
+    document.getElementById("fgd").innerHTML=P20;
+    document.getElementById("fgrdu").innerHTML=P21;
+    document.getElementById("fclgname").innerHTML=P22;
+    document.getElementById("fgd").innerHTML=P23;
+
+
+}
